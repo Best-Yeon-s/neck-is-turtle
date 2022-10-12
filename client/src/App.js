865c162efa21f8a42cf36097a40ae70a619a30f6
moonchaeyeon <sso07069@gmail.com>
2022-10-12 14:37:56 +0900

    [feat] post 측정 기능 구현

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MeasuerPose from './MeasurePose';
+
+function App() {
+  return (
+    <div className="App">
+      <MeasuerPose />
+    </div>
+  );
+}
+
+export default App;
