432776fcf37b5d2e3f2485bf953526ef7b824811
HyeyeonLee <dlgpdus0416@naver.comm>
2022-10-20 10:09:20 +0900

    서버 초기 세팅

---
+++ server/src/main/java/com/example/neckisturtleback/NeckIsTurtleBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.neckisturtleback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeckIsTurtleBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeckIsTurtleBackApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/neckisturtleback/NeckIsTurtleBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neckisturtleback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeckIsTurtleBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
